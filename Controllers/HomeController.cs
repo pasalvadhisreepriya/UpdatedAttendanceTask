d7538dd2f50e9069f9ad5873c823680e245b91e5
pasalvadhisreepriya <105185466+pasalvadhisreepriya@users.noreply.github.com>
2023-08-05 22:11:06 +0530

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,97 @@
-﻿
+//Pseudo code
+// Controller HomeController
+//     // Constants
+//     API_KEY = "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"
+//     MAX_TOKENS = 3500
+//     TEMPERATURE = 0.2
+
+//     // Dependencies
+//     ILogger _logger
+
+//     // Constructor
+//     HomeController(ILogger logger)
+//         _logger = logger
+
+//     // Action: Index
+//     Action Index()
+//         model = new Model()
+//         If Session contains "GeneratedResponse" Then
+//             generatedResponse = Get the value from Session with key "GeneratedResponse"
+//             model.Response = Convert generatedResponse to string using UTF8 encoding
+//         End If
+
+//         Return View with model
+//     End Action
+
+//     // Action: Create
+//     Action Create(Model model)
+//         Try
+//             If model.Prompt is empty AND model.File is null Then
+//                 AddModelError("Please provide a prompt or upload a file.")
+//                 model1 = new Model with Response = "No prompt provided."
+//                 Return View("Index", model1)
+//             End If
+
+//             fileData = null
+//             If model.File is not null Then
+//                 Open a StreamReader with model.File.OpenReadStream()
+//                 Read data from the uploaded file using the StreamReader and store it in fileData
+//             End If
+
+//             messages = Create a List of dynamic objects
+//             Add a dynamic object with role = "system" and content = " " to messages
+//             Add a dynamic object with role = "user" and content = model.Prompt + "\n" + fileData (if fileData is not null) to messages
+
+//             options = Create an anonymous object with properties:
+//                 model = "gpt-3.5-turbo"
+//                 messages = messages
+//                 max_tokens = MAX_TOKENS
+//                 temperature = TEMPERATURE
+
+//             json = Serialize options to JSON
+//             content = Create a StringContent object with json, Encoding.UTF8, and "application/json"
+
+//             Set the Authorization header of client's DefaultRequestHeaders to use the API_KEY
+
+//             response = Send a POST request to "https://api.openai.com/v1/chat/completions" with content using the client
+//             Ensure the response is successful (Status code 200)
+
+//             responseBody = Read the response content as a string
+//             jsonResponse = Deserialize responseBody as dynamic JSON object
+//             result = Get the content property from jsonResponse.choices[0].message as a string
+
+//             model.Response = result
+
+//             Convert model.Response to bytes using UTF8 encoding and store it in generatedResponse
+//             Set the generatedResponse in Session with key "GeneratedResponse"
+
+//             Redirect to the Index action
+//         Catch Exception as ex
+//             Log the error message in _logger
+//             Return Content("An error occurred.")
+//         End Try
+//     End Action
+
+//     // Action: DownloadResponse
+//     Action DownloadResponse()
+//         If Session contains "GeneratedResponse" Then
+//             generatedResponse = Get the value from Session with key "GeneratedResponse"
+//             responseCsv = Convert generatedResponse to string using UTF8 encoding
+
+//             Convert responseCsv to bytes using UTF8 encoding and store it in responseBytes
+//             Generate a random file name and store it in responseFileName
+//             Set the Content-Disposition header to "attachment; filename=" + responseFileName
+//             Add the Content-Disposition header to the response
+
+//             Return a FileContentResult with responseBytes and "text/csv" content type
+//         Else
+//             Return Content("Response not found.")
+//         End If
+//     End Action
+// End Controller
+
+
+
 using UpdatedAttendanceTask.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Net.Http.Headers;
